a885f892b25197a0519e85e3baf76309f22db626
TedMeadow <fl18524a@gmail.com>
2022-02-08 20:07:24 +0300

    something

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory='templates')
 
-@app.get('/')
-async def searcher():
+@app.get('/', response_class='HTMLResponse')
+async def searcher(request: Request):
+    return templates.TemplateResponse('search.html', {'request': request})
+
+@app.post('/', response_class='HTMLResponse')
+async def search_answer(requset: Request, search_request: str = Form()):
     pass
